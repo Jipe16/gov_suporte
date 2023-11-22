9248fa03af1802130c6ca3aec94abb937ad815f8
Jipe <109040883+Jipe16@users.noreply.github.com>
2023-11-22 10:43:42 -0300

    Delete front_end directory

--- front_end/js/criarchamado.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("env-arquivo").onchange = ()=>{
-    let ul = document.getElementById("lista");
-    let files = document.getElementById("env-arquivo").files;
-    for(let i = 0;i<files.length;i++){
-      let li = document.createElement("li");
-      li.appendChild(document.createTextNode(files[i].name));
-      ul.appendChild(li);
-    }
-  }
